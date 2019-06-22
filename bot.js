0fa2613bc206938d338f06d997ff30023052593c
_StrangeR_ <52105016+StrangeR216@users.noreply.github.com>
2019-06-23 01:35:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,10 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
 });
-
 //THIS MUST BE THIS WAY
 client.login(process.env.BOT_TOKEN);
